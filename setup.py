a7d1533f55984e805efd7b02a32a1ca62eedc6f9
Huzaifa Usman <huzaifausman929@gamil.com>
2025-11-25 13:47:03 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="information_retriever",
+    version="0.0.0",
+    author = "Huzaifa",
+    author_email = "zaif@ineuron.ai",
+    packages=find_packages(),
+    install_requires=[]
+)
